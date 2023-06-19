20b4ff015ee678e63b3dd1c6d619635800fd98f8
Sebastien Lacour <sebastien.lacour1994@gmail.com>
2023-06-19 16:39:01 +0200

    premiers tests fetch

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("hello")
+let data = []
+
+const fetchData = async() => {
+    await fetch("https://eldenring.fanapis.com/api/items")
+    .then((response) => response.json())
+    .then((results => {
+        let arrayOfData = results.data
+        console.log(arrayOfData)
+        arrayOfData.forEach(item => {
+            data.push(item)
+        });
+    }))
+    .catch( error => console.error(error))
+}
+
+fetchData()
+
+const display = async() => {
+    await fetchData()
+    console.log(data.length)
+}
+
+display()
